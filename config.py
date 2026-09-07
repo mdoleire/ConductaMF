84d4c6f6de7d058a40fb09b36fd1961cc1770f8e
mdoleire <51235290+mdoleire@users.noreply.github.com>
2026-09-07 17:38:21 -0600

    Rename configuration files for clarity

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # ==========================================
 # 1. CONFIGURACIÓN Y CATÁLOGO
 # ==========================================
-FILE_ALUMNOS = "Alumnos_por_Grupo"
-FILE_ASIGNACIONES = "Asignaciones_Profesores"
-FILE_SEGURIDAD = "Usuarios_Seguridad"
-FILE_REGISTROS = "Base_Conducta_Registros"
-FILE_ASISTENCIA = "Registro_Asistencia"
+FILE_ALUMNOS = "1_Alumnos_por_Grupo"
+FILE_ASIGNACIONES = "2_Asignaciones_Profesores"
+FILE_SEGURIDAD = "3_Usuarios_Seguridad"
+FILE_REGISTROS = "4_Base_Conducta_Registros"
+FILE_ASISTENCIA = "5_Registro_Asistencia"
 
 # config.py
 
